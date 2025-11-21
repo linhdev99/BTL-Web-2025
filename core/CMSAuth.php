b03024fde01cd175b3d0402acc75ae3bc5cea21e
LinhDev <linh.huynh.dev.99@gmail.com>
2025-11-21 15:27:05 +0700

    update home

--- core/CMSAuth.php
+++ core/CMSAuth.php
@@ -16,12 +16,34 @@ class CMSAuth
     }
 
     /**
+     * Kiểm tra xem user đã đăng nhập chưa
+     */
+    public static function checkuser()
+    {
+        if (!isset($_SESSION['user'])) {
+            header("Location: /login");
+            exit;
+        }
+    }
+
+    /**
      * Nếu đã login rồi thì không cho vào trang login/register nữa
      */
     public static function redirectIfLoggedIn()
     {
         if (isset($_SESSION['cms_user'])) {
             header("Location: /cms");
+            exit;
+        }
+    }
+
+    /**
+     * Nếu đã login rồi thì không cho vào trang login/register nữa
+     */
+    public static function redirectIfLoggedInForUser()
+    {
+        if (isset($_SESSION['user'])) {
+            header("Location: /home");
             exit;
         }
     }
